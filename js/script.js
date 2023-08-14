48e574f27fcb6a29df934f0ff4f25a04714f6c6f
michellepolo <michellepolo521@gmail.com>
2023-08-13 20:56:12 -0500

    last update aug13

--- js/script.js
+++ js/script.js
@@ -39,4 +39,21 @@ $(document).ready(function(){
     });
   });
   
-  
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+      document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+
+// Scroll to the top when the button is clicked
+document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
